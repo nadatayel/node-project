a107f72537d5029ad7444b3d81883fafcb1a79f9
nadatayel <77934746+nadatayel@users.noreply.github.com>
2021-01-25 22:50:37 +0200

    done

--- controllers/user.js
+++ controllers/user.js
@@ -4,33 +4,57 @@ const { promisify} =require ('util');
 
 const asyncSign = promisify (jwt.sign);
 const user = require ('../models/user');
-const create =(User)=>user.create(User);
+// const create =(User)=>user.create(User);
 
 
-const login= async ({ username, password })=> {
-    const User= await user.findOne({username}).exec();
-    if(!User){
-        throw Error('UN_AUTHENTICATED');
-    }
+// const login= async ({ username, password })=> {
+//     const User= await user.findOne({username}).exec();
+//     if(!User){
+//         throw Error('UN_AUTHENTICATED');
+//     }
 
-    const isValidPass= User.validatePassword(password);
-    if(isValidPass){
-        console.log( isValidPass);
-        throw Error('UN_AUTHENTICATED2');
-    }
+//     const isValidPass= User.validatePassword(password);
+//     if(isValidPass){
+//         console.log( isValidPass);
+//         throw Error('UN_AUTHENTICATED2');
+//     }
 
-    const token= await asyncSign(
-        { 
-            username: User.username,
-            id: User.id,
-        },
-        'SECRET_MUST_BE_COMPLEX',
-        { expiresIn: '1d' }
-    );
+//     const token= await asyncSign(
+//         { 
+//             username: User.username,
+//             id: User.id,
+//         },
+//         'SECRET_MUST_BE_COMPLEX',
+//         { expiresIn: '1d' }
+//     );
 
-    return { ...user.toJSON(), token};
 
-}
+
+
+const create = (user) => User.create(user);
+
+
+const login = async ({ username, password }) => {
+const user = await User.findOne({ username }).exec();
+
+  if (!user) {
+    throw Error('UN_AUTHENTICATED');
+  }
+
+  const isVaildPass = user.validatePassword(password);
+  console.log(isVaildPass)
+  if (!isVaildPass) {
+    throw Error('UN_AUTHENTICATED');
+  }
+
+  const token = await asyncSign({
+    username: user.username,
+    id: user.id,
+  }, 'SECRET_MUST_BE_COMPLEX', { expiresIn: '1d' } );
+  return { ...user.toJSON(), token };
+};
+    
+
 
 
 const getAll=()=> user.find({}).exec();
